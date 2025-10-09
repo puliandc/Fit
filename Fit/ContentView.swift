6d5c5c104d818f6d8b3757eb8d36eba8efd5217f
puliandc <8724198@qq.com>
2025-10-09 18:25:29 +0800

    Initial Commit

---
+++ Fit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fit
+//
+//  Created by 陆家贤 on 9/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fit/FitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitApp.swift
+//  Fit
+//
+//  Created by 陆家贤 on 9/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct FitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
