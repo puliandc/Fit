808a002f0f5c75efa1927f7a27c43f64c9f96f6a
puliandc <8724198@qq.com>
2025-10-31 09:46:11 +0800

    再次尝试性能优化。

--- fit-react/fit-react/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.fitapp.app',
-  appName: 'FitApp',
-  webDir: 'dist'
-};
-
-export default config;
--- fit-react/fit-react/postcss.config.js
+++
@@ -1,7 +0,0 @@
-//created by Jason Lu on 22:43:00 10/26/2025
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
-}
